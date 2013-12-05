265ba128d5560da28ba633821395e29dbc897a51
panbin <panbin2010@163.com>
2013-12-05 13:54:36 +0800

    在VideoPlayerKit基础上修改
    播放视频

---
+++ PlayVideoDemo/BTVideoPlayerKitViewController.h
@@ -0,0 +1,13 @@
+//
+//  BTVideoPlayerKitViewController.h
+//  PlayVideoDemo
+//
+//  Created by panbin on 13-12-5.
+//  Copyright (c) 2013年 Handpay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BTVideoPlayerKitViewController : UIViewController
+
+@end
---
+++ PlayVideoDemo/BTVideoPlayerKitViewController.m
@@ -0,0 +1,38 @@
+//
+//  BTVideoPlayerKitViewController.m
+//  PlayVideoDemo
+//
+//  Created by panbin on 13-12-5.
+//  Copyright (c) 2013年 Handpay. All rights reserved.
+//
+
+#import "BTVideoPlayerKitViewController.h"
+
+@interface BTVideoPlayerKitViewController ()
+
+@end
+
+@implementation BTVideoPlayerKitViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
